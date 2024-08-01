32bfb57847427097db1e3c9c425e25798512f312
mzp <me@mzp.jp>
2024-07-31 21:57:03 -0700

    preference app

---
+++ AquaSKK-PreferencesTests/AquaSKK_PreferencesTests.swift
@@ -0,0 +1,17 @@
+//
+//  AquaSKK_PreferencesTests.swift
+//  AquaSKK-PreferencesTests
+//
+//  Created by mzp on 7/31/24.
+//
+
+import Testing
+@testable import AquaSKK_Preferences
+
+struct AquaSKK_PreferencesTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
