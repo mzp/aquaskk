249acd65bb973e9aea1d8fe5b64f1c43387876fd
mzp <me@mzp.jp>
2024-08-05 21:04:51 +0900

    Add ui framework

---
+++ Sources/UI/AquaSKKUI.h
@@ -0,0 +1,18 @@
+//
+//  AquaSKKUI.h
+//  AquaSKKUI
+//
+//  Created by mzp on 8/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AquaSKKUI.
+FOUNDATION_EXPORT double AquaSKKUIVersionNumber;
+
+//! Project version string for AquaSKKUI.
+FOUNDATION_EXPORT const unsigned char AquaSKKUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AquaSKKUI/PublicHeader.h>
+
+
---
+++ Tests/UITests/AquaSKKUITests.swift
@@ -0,0 +1,17 @@
+//
+//  AquaSKKUITests.swift
+//  AquaSKKUITests
+//
+//  Created by mzp on 8/5/24.
+//
+
+import Testing
+@testable import AquaSKKUI
+
+struct AquaSKKUITests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
