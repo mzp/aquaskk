8c6767dced0125c8bdd199c43f59209d36063990
mzp <me@mzp.jp>
2024-08-06 06:00:32 +0900

    format

--- Tests/UITests/AquaSKKUITests.swift
+++ Tests/UITests/AquaSKKUITests.swift
@@ -5,13 +5,11 @@
 //  Created by mzp on 8/5/24.
 //
 
-import Testing
 @testable import AquaSKKUI
+import Testing
 
 struct AquaSKKUITests {
-
     @Test func testExample() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
