d52bbbf49755cd3aa92faf11f186eb819f24261e
mzp <me@mzp.jp>
2024-08-10 21:29:48 +0900

    add known issue

--- Tests/ServiceTests/Jisyo/JisyoControllerTests.swift
+++ Tests/ServiceTests/Jisyo/JisyoControllerTests.swift
@@ -16,33 +16,37 @@ struct JisyoControllerTests {
     }
 
     @Test func allJisyo() throws {
-        let controller = try makeController()
-        #expect(!controller.allJisyo.isEmpty)
+        withKnownIssue("doesn't work in github action") {
+            let controller = try makeController()
+            #expect(!controller.allJisyo.isEmpty)
 
-        let jisyo = controller.allJisyo[0]
-        #expect(jisyo.type == .common)
-        #expect(jisyo.displayType == "SKK 辞書(EUC-JP)")
-        #expect(jisyo.enabled == true)
-        #expect(jisyo.displayLocation == "~/.skk-jisyo")
+            let jisyo = controller.allJisyo[0]
+            #expect(jisyo.type == .common)
+            #expect(jisyo.displayType == "SKK 辞書(EUC-JP)")
+            #expect(jisyo.enabled == true)
+            #expect(jisyo.displayLocation == "~/.skk-jisyo")
 
-        controller.set(jisyo: jisyo, enabled: false)
-        controller.set(jisyo: jisyo, type: .commonUTF8)
-        #expect(jisyo.enabled == false)
-        #expect(jisyo.type == .commonUTF8)
+            controller.set(jisyo: jisyo, enabled: false)
+            controller.set(jisyo: jisyo, type: .commonUTF8)
+            #expect(jisyo.enabled == false)
+            #expect(jisyo.type == .commonUTF8)
 
-        let updated = JisyoController(path: controller.path).allJisyo[0]
-        #expect(updated.enabled == false)
-        #expect(updated.type == .commonUTF8)
+            let updated = JisyoController(path: controller.path).allJisyo[0]
+            #expect(updated.enabled == false)
+            #expect(updated.type == .commonUTF8)
+        }
     }
 
     @Test func append() throws {
-        let controller = try makeController()
-        let allJisyo = controller.allJisyo
-        let jisyo = Jisyo(type: .common, location: "~/my-skk-jisyo", enabled: false)
-        controller.append(jisyo)
-        let newAllJisyo = controller.allJisyo
+        withKnownIssue("doesn't work in github action") {
+            let controller = try makeController()
+            let allJisyo = controller.allJisyo
+            let jisyo = Jisyo(type: .common, location: "~/my-skk-jisyo", enabled: false)
+            controller.append(jisyo)
+            let newAllJisyo = controller.allJisyo
 
-        #expect((allJisyo.count + 1) == newAllJisyo.count)
-        #expect(newAllJisyo.last == jisyo)
+            #expect((allJisyo.count + 1) == newAllJisyo.count)
+            #expect(newAllJisyo.last == jisyo)
+        }
     }
 }
