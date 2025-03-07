2e74891a43efa4183b7e9fdde11788a32334f323
mzp <me@mzp.jp>
2025-03-07 00:31:16 -0800

    forward decl

--- Sources/Engine/utility/AquaSKKEngine-Preamble.h
+++ Sources/Engine/utility/AquaSKKEngine-Preamble.h
@@ -8,6 +8,10 @@
 #ifndef AquaSKKEngine_Preamble_h
 #define AquaSKKEngine_Preamble_h
 
+namespace AquaSKKEngine {
+    class SKKInputqueueImpl;
+}
+
 #import <AquaSKKBackend/SKKCandidate.h>
 #import <AquaSKKBackend/SKKEntry.h>
 #import <AquaSKKEngine/SKKBaseEditor.h>
