4ffb3d1b79e287834f8617416d7c63ee9c9e48aa
mzp <me@mzp.jp>
2025-03-23 17:02:18 -0700

    swiftformat

--- Tests/SKKServTests/SKKServTests.swift
+++ Tests/SKKServTests/SKKServTests.swift
@@ -10,8 +10,8 @@ internal import AquaSKKTesting
 import AquaSKKService
 @_spi(Testing) internal import AquaSKKBackend
 
-// skkservを起動するため、他のテストと並列実行すると結果が不安定になる
-// それを避けるために独立したテストターゲットにした上で直列実行する。
+/// skkservを起動するため、他のテストと並列実行すると結果が不安定になる
+/// それを避けるために独立したテストターゲットにした上で直列実行する。
 @Suite(.serialized) struct SKKServTests {
     @Test func launch() async throws {
         let proxy = SKKServerProxy()
