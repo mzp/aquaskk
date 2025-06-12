3ea923fb60dc60489157bb6c6dcdc93925457f12
mzp <me@mzp.jp>
2025-06-11 22:20:20 -0700

    remove adapter

--- Sources/Engine/AquaSKKEngine.h
+++ Sources/Engine/AquaSKKEngine.h
@@ -11,16 +11,6 @@ FOUNDATION_EXPORT double AquaSKKEngineVersionNumber;
 
 FOUNDATION_EXPORT const unsigned char AquaSKKEngineVersionString[];
 
-#pragma mark - Adapter
-#import <AquaSKKEngine/SKKAnnotatorAdapter.h>
-#import <AquaSKKEngine/SKKCandidateWindowAdapter.h>
-#import <AquaSKKEngine/SKKClipboardAdapter.h>
-#import <AquaSKKEngine/SKKConfigAdapter.h>
-#import <AquaSKKEngine/SKKDynamicCompletorAdapter.h>
-#import <AquaSKKEngine/SKKFrontEndAdapter.h>
-#import <AquaSKKEngine/SKKInputModeListenerAdapter.h>
-#import <AquaSKKEngine/SKKMessengerAdapter.h>
-
 #pragma mark - bridge
 
 #import <AquaSKKEngine/SKKAnnotator.h>
